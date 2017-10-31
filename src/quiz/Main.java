026fd7fb230f2dbb7eb370e415c999d68119dfb4
Estudiante <Estudiante@10.203.2.121>
2017-10-31 17:00:06 -0500

    Merge origin/master
    Conflicts:
    	src/proyecto/Tablero.java

--- src/quiz/Main.java
+++ src/quiz/Main.java
@@ -15,6 +15,47 @@ public class Main {
     public static void main(String[] args) {
         WebGallery w = new WebGallery();
         Scanner teclado = new Scanner(System.in);
-        System.out.println("si desea");
+        int x = 0;
+        String n = "";
+        String c = "";
+        
+        while(true){
+          System.out.println("si desea crear un cuenta ingrese 1 si desea inisiar sesion 2");  
+          x=teclado.nextInt(x);
+          if(x==1){
+             while (true){
+                System.out.println("ingrse 1 para artista o 2 para comprador");
+              x=teclado.nextInt();
+              if(x==1){
+                  System.out.println("ingrese el nombre y la contraseña");
+                  n=teclado.next();
+                  c=teclado.next();
+                  w.NuevoArtista(n,c);
+                  
+                }
+              else if (x==2){
+                  System.out.println("ingrese el nombre y la contraseña");
+                  n=teclado.next();
+                  c=teclado.next();
+                  w.NuevoComprador(n,c);
+                 
+              }
+              else{
+                  System.out.println("digito no valido");
+               }
+              }
+          }
+         else if (x==2){
+                  
+            }
+          
+          else if(x==2){
+              
+          }
+          else{
+              System.out.println("digito no valido");
+          }
+        }
+        
     }
 }
